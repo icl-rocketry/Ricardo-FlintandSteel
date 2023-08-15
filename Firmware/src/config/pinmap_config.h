3baa8b21c74eb3dda523164430891290a7c8ce88
Andrei Paduraru <83088926+Padure3@users.noreply.github.com>
2023-08-15 12:06:28 +0100

    adding firmware finally

---
+++ Firmware/src/config/pinmap_config.h
@@ -0,0 +1,35 @@
+/*
+**********************
+* PINS               *
+**********************
+ */
+#pragma once
+
+namespace PinMap{
+    static constexpr int Nuke1 = 35;
+    static constexpr int Nuke2 = 34;
+    static constexpr int Nuke3 = 33;
+    static constexpr int Nuke4 = 21;
+
+    static constexpr int Cont1 = 36;
+    static constexpr int Cont2 = 47;
+    static constexpr int Cont3 = 38;
+    static constexpr int Cont4 = 37;
+
+
+    static constexpr int TxCan = 32;
+    static constexpr int RxCan = 33;
+
+    // static constexpr int TxCan = 27;
+    // static constexpr int RxCan = 14;
+
+
+    // static constexpr int TxCan = 33;
+    // static constexpr int RxCan = 32;
+    static constexpr int SdDet_1 = 34;
+    static constexpr int SdDet_2 = 35;
+    static constexpr int BattVolt = 36;
+    static constexpr int LoraInt = 39;
+};
+
+
