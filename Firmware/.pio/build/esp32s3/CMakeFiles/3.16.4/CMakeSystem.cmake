d522bbd7c9ae5d17b787a4df2576d59af14ac195
Andrei <paduraru.andrei5@gmail.com>
2024-09-04 02:02:20 +0100

    v3 hardware I guess

--- Firmware/.pio/build/esp32s3/CMakeFiles/3.16.4/CMakeSystem.cmake
+++ Firmware/.pio/build/esp32s3/CMakeFiles/3.16.4/CMakeSystem.cmake
@@ -1,6 +1,6 @@
-set(CMAKE_HOST_SYSTEM "Darwin-23.1.0")
+set(CMAKE_HOST_SYSTEM "Darwin-23.4.0")
 set(CMAKE_HOST_SYSTEM_NAME "Darwin")
-set(CMAKE_HOST_SYSTEM_VERSION "23.1.0")
+set(CMAKE_HOST_SYSTEM_VERSION "23.4.0")
 set(CMAKE_HOST_SYSTEM_PROCESSOR "x86_64")
 
 include("/Users/andrei/.platformio/packages/framework-espidf/tools/cmake/toolchain-esp32s3.cmake")
